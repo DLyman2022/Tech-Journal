39b2fb78cae3940cd05b548ebcf14959f3652a30
DLyman2022 <damion.lyman@mymail.champlain.edu>
2020-02-20 17:01:39 -0500

    script

---
+++ SYS265_20/linux/centos7/secure-ssh.sh
@@ -0,0 +1,6 @@
+#secure-ssh.sh
+#author DLyman (DLyman2022)
+#creates a new ssh user using $l parameter
+#adds a public key from the local repo or curled from the remote repo
+#removes roots ability to ssh in
+echo "ALL YOUR CODE GOES HERE"
